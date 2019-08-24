7f51b202b1b4f4c4a7b8dfaa9c3c6d0cc3c9a4b4
Wojciech Buczko <Wojtek120PL@gmail.com>
2019-08-24 23:41:03 +0200

    Add documentation to ExerciseDao
    * added documentation to ExerciseDao

--- src/main/java/pl/wojtek120/school/dao/ExerciseDao.java
+++ src/main/java/pl/wojtek120/school/dao/ExerciseDao.java
@@ -15,6 +15,12 @@ public class ExerciseDao {
     private static final String FIND_ALL_EXERCISES_QUERY = "SELECT * FROM exercise";
     private static final String FIND_ALL_EXERCISES_THAT_ARE_NOT_SOLVED_BY_USER_QUERY = "SELECT * FROM exercise WHERE NOT id IN (SELECT exercise_id FROM solution WHERE user_id = ?);";
 
+    /**
+     * Function to create new exercise record
+     *
+     * @param exercise exercise data model
+     * @return created exercise model
+     */
     public Exercise create(Exercise exercise) {
         try (Connection connection = DbUtil.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(CREATE_EXERCISE_QUERY, Statement.RETURN_GENERATED_KEYS);
@@ -35,13 +41,19 @@ public class ExerciseDao {
         return exercise;
     }
 
+    /**
+     * Function to read record by chosen id
+     *
+     * @param id id of record to read
+     * @return object of model that was read
+     */
     public Exercise read(int id) {
 
         try (Connection connection = DbUtil.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(READ_EXERCISE_QUERY);
             statement.setInt(1, id);
 
-            try(ResultSet resultSet = statement.executeQuery()) {
+            try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
                     Exercise exercise = new Exercise();
                     exercise.setId(resultSet.getInt("id"));
@@ -57,6 +69,11 @@ public class ExerciseDao {
         return null;
     }
 
+    /**
+     * Function to update record
+     *
+     * @param exercise object to be updated
+     */
     public void update(Exercise exercise) {
         try (Connection connection = DbUtil.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(UPDATE_EXERCISE_QUERY);
@@ -70,6 +87,11 @@ public class ExerciseDao {
         }
     }
 
+    /**
+     * Function to delete record
+     *
+     * @param id id of record to be deleted
+     */
     public void delete(int id) {
         try (Connection connection = DbUtil.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(DELETE_EXERCISE_QUERY);
@@ -80,20 +102,39 @@ public class ExerciseDao {
         }
     }
 
+    /**
+     * Function to return list of all exercise records from database
+     *
+     * @return list of all records
+     */
     public List<Exercise> findAll() {
         return findAllExecuteQuery(FIND_ALL_EXERCISES_QUERY, -1);
     }
 
+    /**
+     * Function to return list of all exercise records that are not solved
+     * and belongs to specified in parameter user
+     *
+     * @param userId id of user wo which solution must belong
+     * @return list of records
+     */
     public List<Exercise> findAllThatAreNotSolved(int userId) {
         return findAllExecuteQuery(FIND_ALL_EXERCISES_THAT_ARE_NOT_SOLVED_BY_USER_QUERY, userId);
     }
 
-    private List<Exercise> findAllExecuteQuery(String query, int id){
+
+    /**
+     * Function to return list of data, by specified query
+     *
+     * @param id id to be set in query. If id <= 0, than skipped.
+     * @return list of data
+     */
+    private List<Exercise> findAllExecuteQuery(String query, int id) {
         try (Connection connection = DbUtil.getConnection()) {
             List<Exercise> exercises = new ArrayList<>();
             PreparedStatement statement = connection.prepareStatement(query);
 
-            if(id > 0) {
+            if (id > 0) {
                 statement.setInt(1, id);
             }
 
@@ -109,7 +150,13 @@ public class ExerciseDao {
         }
     }
 
-    private Exercise putDataFromResultSetIntoSolution(ResultSet resultSet){
+    /**
+     * Function to put data from result set to model
+     *
+     * @param resultSet result set
+     * @return model with data
+     */
+    private Exercise putDataFromResultSetIntoSolution(ResultSet resultSet) {
         Exercise exercise = new Exercise();
         try {
             exercise.setId(resultSet.getInt("id"));
